050a08ad606f942430c403f62751c1f6f126940c
claudiogodoy99 <cavalcantigodoy@gmail.com>
2024-03-24 11:27:46 -0300

    ThreadPool

---
+++ demos/demo_thread_pool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        // Criar uma tarefa usando Task.Factory.StartNew
+        Task task1 = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("Tarefa 1 iniciada na thread " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000); // Simula algum processamento
+            Console.WriteLine("Tarefa 1 concluída na thread " + Thread.CurrentThread.ManagedThreadId);
+        });
+
+        // Criar uma tarefa usando Task.Run
+        Task task2 = Task.Run(() =>
+        {
+            Console.WriteLine("Tarefa 2 iniciada na thread " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000); // Simula algum processamento
+            Console.WriteLine("Tarefa 2 concluída na thread " + Thread.CurrentThread.ManagedThreadId);
+        });
+
+        // Esperar que as tarefas terminem antes de encerrar o programa
+        Task.WaitAll(task1, task2);
+
+        Console.WriteLine("Todas as tarefas concluídas. Pressione qualquer tecla para sair.");
+        Console.ReadKey();
+    }
+}
---
+++ demos/demo_thread_pool_no_aspnet/Threadpool_Aspnet/Controllers/ThreadsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Threadpool_Aspnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThreadsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            var threadPoolQueue = ThreadPool.PendingWorkItemCount;
+            var threadNumber = ThreadPool.ThreadCount;
+
+
+            var msg = $"Requisição processada pela thread: {threadId} \n ThreadPool numero de threads: {threadNumber} \n ThreadPool Tasks Pendendetes: {threadPoolQueue}";
+            Console.WriteLine(msg);
+            return Ok(msg);
+        }
+    }
+}
