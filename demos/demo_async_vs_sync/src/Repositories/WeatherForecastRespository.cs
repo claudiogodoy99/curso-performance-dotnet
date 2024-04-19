51cfd7bbda31ba052a43bd438413c8813398f54a
claudiogodoy99 <cavalcantigodoy@gmail.com>
2024-04-19 06:57:58 -0300

    async await load test

---
+++ demos/demo_async_vs_sync/src/Repositories/WeatherForecastRespository.cs
@@ -0,0 +1,45 @@
+public class WeatherForecastRepository {
+    
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> Get(){
+        // Simula uma chamada ao banco de dados que demora 300 ms
+        Thread.Sleep(300);
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+    }
+
+    public async Task<IEnumerable<WeatherForecast>> GetAsync(){
+        // Simula uma chamada ao banco de dados que demora 300 ms de forma assincrona
+        await Task.Delay(300);
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+    }
+}
+
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
